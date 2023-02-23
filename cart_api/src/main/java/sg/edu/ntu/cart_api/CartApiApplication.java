ef2c2e177565b9aee8614e7ad9e6b0158511c568
Edison Zhuang <edisen.zhuang@gmail.com>
2023-02-23 23:14:17 +0800

    completed code

--- cart_api/src/main/java/sg/edu/ntu/cart_api/CartApiApplication.java
+++ cart_api/src/main/java/sg/edu/ntu/cart_api/CartApiApplication.java
@@ -1,13 +1,24 @@
 package sg.edu.ntu.cart_api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import sg.edu.ntu.cart_api.helper.MinimumPayableCheckHelper;
 
 @SpringBootApplication
 public class CartApiApplication {
+
+	@Value("${MIN_PURCHASE}")
+    float minimumPurchase;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartApiApplication.class, args);
 	}
 
+	@Bean
+	public MinimumPayableCheckHelper minimumPayableCheckHelper(){
+		return new MinimumPayableCheckHelper(minimumPurchase);
+	}
 }
